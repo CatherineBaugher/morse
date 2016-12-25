3c146c8b5dd4f753d4d705356cf49b921bda694b
Mockasin <liamalover3>
2016-12-25 13:53:03 -0500

    implemented morse standard
    also added a reference from wiki and input text for later

--- morse.ino
+++ morse.ino
@@ -16,83 +16,216 @@ void setup() {
 void transMorse(char inp){
   switch(inp){
     case 'a':
+      dot();
+      dash();
       break;
     case 'b':
+      dash();
+      dot();
+      dot();
+      dot();
       break;
     case 'c':
+      dash();
+      dot();
+      dash();
+      dot();
       break;
     case 'd':
+      dash();
+      dot();
+      dot();
       break;
     case 'e':
+      dot();
       break;
     case 'f':
+      dot();
+      dot();
+      dash();
+      dot();
       break;
     case 'g':
+      dash();
+      dash();
+      dot();
       break;
     case 'h':
+      dot();
+      dot();
+      dot();
+      dot();
       break;
     case 'i':
+      dot();
+      dot();
       break;
     case 'j':
+      dot();
+      dash();
+      dash();
+      dash();
       break;
     case 'k':
+      dash();
+      dot();
+      dash();
       break;
     case 'l':
+      dot();
+      dash();
+      dot();
+      dot();
       break;
     case 'm':
+      dash();
+      dash();
       break;
     case 'n':
+      dash();
+      dot();
       break;
     case 'o':
+      dash();
+      dash();
+      dash();
       break;
     case 'p':
+      dot();
+      dash();
+      dash();
+      dot();
       break;
     case 'q':
+      dash();
+      dash();
+      dot();
+      dash();
       break;
     case 'r':
+      dot();
+      dash();
+      dot();
       break;
     case 's':
+      dot();
+      dot();
+      dot();
       break;
     case 't':
+      dash();
       break;
     case 'u':
+      dot();
+      dot();
+      dash();
       break;
     case 'v':
+      dot();
+      dot();
+      dot();
+      dash();
       break;
     case 'w':
+      dot();
+      dash();
+      dash();
       break;
     case 'x':
+      dash();
+      dot();
+      dot();
+      dash();
       break;
     case 'y':
+      dash();
+      dot();
+      dash();
+      dash();
       break;
     case 'z':
+      dash();
+      dash();
+      dot();
+      dot();
       break;
     case '1':
+      dot();
+      dash();
+      dash();
+      dash();
+      dash();
       break;
     case '2':
+      dot();
+      dot();
+      dash();
+      dash();
+      dash();
       break;
     case '3':
+      dot();
+      dot();
+      dot();
+      dash();
+      dash();
       break;
     case '4':
+      dot();
+      dot();
+      dot();
+      dot();
+      dash();
       break;
     case '5':
+      dot();
+      dot();
+      dot();
+      dot();
+      dot();
       break;
     case '6':
+      dash();
+      dot();
+      dot();
+      dot();
+      dot();
       break;
     case '7':
+      dash();
+      dash();
+      dot();
+      dot();
+      dot();
       break;
     case '8':
+      dash();
+      dash();
+      dash();
+      dot();
+      dot();
       break;
     case '9':
+      dash();
+      dash();
+      dash();
+      dash();
+      dot();
       break;
     case '0':
+      dash();
+      dash();
+      dash();
+      dash();
+      dash();
       break;
     default:
       break;
-  }   
+  }
+  letbreak();
 }
 void dot(){
-  delay(300);
+  delay(300); //one unit
   digitalWrite(led12, HIGH);
   delay(300);
   digitalWrite(led12, LOW);
@@ -100,8 +233,14 @@ void dot(){
 void dash(){
   delay(300);
   digitalWrite(led12, HIGH);
-  delay(900);
+  delay(900); //three units
   digitalWrite(led12, LOW);
+}
+void letbreak(){
+  delay(600); //900-300=600
+}
+void worbreak(){
+  delay(1200); //2100-300-600=1200
 }
 void loop() {
 }
